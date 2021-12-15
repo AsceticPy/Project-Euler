142a3c3a8a6745e99efb77726ad382c1a68163e0
Pierre Sicallac <pierre.sicallac@hotmail.fr>
2021-12-15 11:05:55 +0100

    Create Problem_10.py

---
+++ Problem_10.py
@@ -0,0 +1,23 @@
+"""
+The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+Find the sum of all the primes below two million
+"""
+from itertools import product
+
+
+n = 1999999
+prime_array = [True for i in range(n)]
+
+for i in range(2, int(len(prime_array) ** 0.5)):
+	if prime_array[i] == True:
+		x = 0
+		k = 0
+		for j in range(3, n):
+			if j == i**2 + (x*k):
+				prime_array[j] = False
+				x +=1
+				k = i
+
+sum_of_prime = sum([i for i in range(n) if prime_array[i] == True and i != 0 and i != 1])
+
+print("The sum of all prime number below two millions is : ", sum_of_prime)
