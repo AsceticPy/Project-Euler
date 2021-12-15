59b867e4f86098d9954c5457f52a573ca1112304
Pierre Sicallac <pierre.sicallac@hotmail.fr>
2021-12-15 08:32:12 +0100

    Update Problem_09.py

--- Problem_09.py
+++ Problem_09.py
@@ -5,11 +5,8 @@ For example, 32 + 42 = 9 + 16 = 25 = 52.
 There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 Find the product abc.
 """
-a = 1
-b = 2
-c = 100
+
 solved = False
-print("a = ", a, " b = ", b, " c = ", c)
 for a in range(1, 1000):
 	for b in range(1, 1000):
 		for c in range(1, 1000):
